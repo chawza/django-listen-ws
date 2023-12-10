904b06c899d30db067628e3199da6212dbf8551c
Nabeel KM <nabeelkahlil403@gmail.com>
2023-12-10 12:08:39 +0700

    add webscket server

---
+++ ws/counter.py
@@ -0,0 +1,33 @@
+import asyncio
+import json
+from redis.asyncio import Redis
+
+import websockets as ws
+
+CURR_COUNTER = 'counter:current'
+
+async def handler(websocket):
+    redis = Redis()
+    p = redis.pubsub()
+    await p.subscribe('counter')
+    await redis.set(CURR_COUNTER, 0, nx=True)
+    
+    curr = await redis.get(CURR_COUNTER)
+    await websocket.send(json.dumps({'counter': int(curr)}))
+
+    while True:
+        try:
+            message = await p.get_message()
+            if message is not None:
+                new_counter = message['data']
+                await redis.set(CURR_COUNTER, int(new_counter))
+                await websocket.send(json.dumps({'counter': int(new_counter)}))
+        except ws.ConnectionClosedOK:
+            break
+
+async def main():
+    async with ws.serve(handler, '', '8001'):
+        await asyncio.Future()
+
+if __name__ == "__main__":
+    asyncio.run(main())
