ddd0d8caf001413d2ded45cb93339bf62cb6bb04
Nabeel KM <nabeelkahlil403@gmail.com>
2023-12-10 10:43:53 +0700

    add redis publisher

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+from redis import StrictRedis
+from django import forms
+from redis import Redis
 
 # Create your views here.
+
+def get_redis():
+    return Redis()
+
+class SetCounterForm(forms.Form):
+    counter = forms.IntegerField(label="Counter")
+    
+    def set(self):
+        counter = self.cleaned_data['counter']
+        redis = get_redis()
+        redis.publish('counter', counter)
 
 def index(request):
     return render(request, 'index.html')
 
 def counter(requust):
-    return render(requust, 'counter.html')
+    form = SetCounterForm(data=requust.POST or None)
+    if form.is_valid():
+        form.set()
+    return render(requust, 'counter.html', context={"form": form})
 
